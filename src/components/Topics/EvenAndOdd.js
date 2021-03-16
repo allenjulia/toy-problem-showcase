2ab7db1270040b0cbda576c7520caea9087fb460
Julia Allen <julia.all3n@gmail.com>
2021-03-16 14:42:51 -0600

    Much confusion, some learning, still working

--- src/components/Topics/EvenAndOdd.js
+++ src/components/Topics/EvenAndOdd.js
@@ -2,15 +2,24 @@ import React, {Component} from 'react'
 
 
 export default class EvenAndOdd extends Component {
+  constructor() {
+    super();
+    this.state = {
+      evensArray: [],
+      oddsArray: [],
+      userInput: ""
+    }
+    
+  }
   render(){
     return (
-      <div className="puzzleBox evenAndOddPB">
-        <h4>Evens and Odds</h4>
-        <input className="inputLine"/>
-        <button className="confirmationButton">Split</button>
-        <span className="resultsBox"></span>
-        <span className="resultsBox"></span>
-      </div>
+     <div className="puzzleBox evenAndOddPB">
+       <h4>"Evens and Odds"</h4>
+       <input onChange={(handleChange)} className="inputLine"></input>
+       <button onClick={(handleClick)}className="confirmationButton">Split</button>
+       <span className="resultsBox">Evens: {this.setState.evensArray} </span>
+       <span className="resultsBox">Odds:{this.setState.oddsArray}</span>
+     </div>
     )  
   }
 }
